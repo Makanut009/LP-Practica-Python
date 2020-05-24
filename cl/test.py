568d056821d72f2697c426100dfd59ec44302ec0
Makanut009 <45299503+Makanut009@users.noreply.github.com>
2020-05-24 12:43:10 +0200

    Base

--- cl/test.py
+++ cl/test.py
@@ -2,7 +2,6 @@ import sys
 from antlr4 import *
 from SkylineLexer import SkylineLexer
 from SkylineParser import SkylineParser
-from SkylineVisitor import SkylineVisitor
 
 input_stream = InputStream(input('? '))
 
@@ -10,11 +9,13 @@ lexer = SkylineLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 parser = SkylineParser(token_stream)
 tree = parser.root()
-
-visitor = SkylineVisitor()
-# evaluator = SkylineEvaluator()
-print('Arbre:')
-visitor.visit(tree)
 print(tree.toStringTree(recog=parser))
-# print('Resultat:')
-# evaluator.visit(tree)
+
+# visitor = SkylineVisitor()
+# # evaluator = SkylineEvaluator()
+# print('Arbre:')
+# visitor.visit(tree)
+# print(tree.toStringTree(recog=parser))
+# print(visitor.taula_simbols)
+# # print('Resultat:')
+# # evaluator.visit(tree)
