839b2f76b0788a860dad4327f076a954af33e55b
Makanut009 <45299503+Makanut009@users.noreply.github.com>
2020-05-26 10:30:48 +0200

    Primers intents de save/load

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def help(update, context):
             "/lst: mostra els identificadors definits i la seva corresponent àrea\n" +
             "/clean: esborra tots els identificadors definits\n" +
             "/save id: guarda un skyline amb el nom id.sky\n" +
-            "/counter id: carrega un skyline de l’arxiu id.sky"
+            "/load id: carrega un skyline de l’arxiu id.sky"
     )
 
 
@@ -48,6 +48,54 @@ def counter(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=str(context.user_data['counter']))
+
+def load(update, context):
+    id = ' '.join(context.args)
+    sk = read_pickle(id + ".sky")
+    xs = []
+    alts = []
+    for e in sk.edificis:
+        xs += [i for i in range(e[0], e[2])]
+        n = (e[2] - e[0])
+        alts += [e[1]]*n
+
+    print(len(xs), len(alts))
+    
+    try:
+        plt.bar(xs, alts, width=1, align='edge', color=['red'])
+        print("MAIAIAU")
+        plt.savefig('plot.png')
+        plt.close()
+    except Exception as err:
+        print(traceback.format_exc())
+        print(sys.exc_info())
+    
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=id
+    )
+
+def save(update, context):
+    id = ' '.join(context.args)
+    sk = Skyline(2,3,4)
+    write_pickle(sk, id + ".sky")
+    
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=id
+    )
+
+
+def read_pickle(filename: str):
+    """Read object from pickle file."""
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def write_pickle(obj: object, filename: str):
+    """Save object in pickle file."""
+    with open(filename, "wb") as file:
+        return pickle.dump(obj, file)
 
 
 visitor = EvalVisitor()
@@ -114,18 +162,11 @@ dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('counter', counter))
 dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('author', author))
+dispatcher.add_handler(CommandHandler('load', load))
+dispatcher.add_handler(CommandHandler('save', save))
+
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, aux))
 
 # engega el bot
 updater.start_polling()
-
-
-
-    # try:
-    #     tree = parser.root()
-    # except Exception as err:
-    #     traceback.print_tb(err.__traceback__)
-    #     print(traceback.format_exc())
-    #     # or
-    #     print(sys.exc_info())
