729f68ae51bbdd408e189ec0d5409653f125cc8d
Makanut009 <45299503+Makanut009@users.noreply.github.com>
2020-05-28 09:23:16 +0200

    Codi en PEP8

--- cl/EvalVisitor.py
+++ cl/EvalVisitor.py
@@ -1,10 +1,11 @@
 from antlr4 import *
+from Skyline import Skyline
+
 if __name__ is not None and "." in __name__:
     from .SkylineParser import SkylineParser
 else:
     from SkylineParser import SkylineParser
 
-from Skyline import Skyline
 
 class EvalVisitor(ParseTreeVisitor):
 
@@ -12,35 +13,31 @@ class EvalVisitor(ParseTreeVisitor):
         self.taula_simbols = {}
 
     # Visit a parse tree produced by SkylineParser#root.
-    def visitRoot(self, ctx:SkylineParser.RootContext):
+    def visitRoot(self, ctx: SkylineParser.RootContext):
         r = self.visit(next(ctx.getChildren()))
         return r
 
-
     # Visit a parse tree produced by SkylineParser#instruccio.
-    def visitInstruccio(self, ctx:SkylineParser.InstruccioContext):
+    def visitInstruccio(self, ctx: SkylineParser.InstruccioContext):
         if next(ctx.getChildren()) == ctx.assig():
             return self.visit(next(ctx.getChildren()))
         else:
             return (None, self.visit(next(ctx.getChildren())))
 
-
     # Visit a parse tree produced by SkylineParser#assig.
-    def visitAssig(self, ctx:SkylineParser.AssigContext):
+    def visitAssig(self, ctx: SkylineParser.AssigContext):
         res = self.visit(ctx.expr())
         self.taula_simbols[ctx.VAR().getText()] = res
         return (ctx.VAR().getText(), res)
 
-
     # Visit a parse tree produced by SkylineParser#expr.
-    def visitExpr(self, ctx:SkylineParser.ExprContext):
+    def visitExpr(self, ctx: SkylineParser.ExprContext):
         if ctx.getChildCount() == 1:
             return self.visit(next(ctx.getChildren()))
             # ch = next(ctx.getChildren())
             # if ch == ctx.simbol():
             #     return self.visit(ctx.simbol())
             # elif ch == ctx.simbol():
-                
 
         elif ctx.getChildCount() == 2:
             return - self.visit(ctx.simbol())
@@ -64,9 +61,8 @@ class EvalVisitor(ParseTreeVisitor):
                 sk = self.visit(l[0]) - self.visit(l[2])
                 return skç
 
-
     # Visit a parse tree produced by SkylineParser#simbol.
-    def visitSimbol(self, ctx:SkylineParser.SimbolContext):
+    def visitSimbol(self, ctx: SkylineParser.SimbolContext):
         if ctx.VAR() is not None:
             return self.taula_simbols[ctx.getText()]
         elif ctx.NUM() is not None:
@@ -75,16 +71,14 @@ class EvalVisitor(ParseTreeVisitor):
             edifici = self.visit(ctx.edifici())
             return Skyline(edifici[0], edifici[1], edifici[2])
 
-
     # Visit a parse tree produced by SkylineParser#edifici.
-    def visitEdifici(self, ctx:SkylineParser.EdificiContext):
+    def visitEdifici(self, ctx: SkylineParser.EdificiContext):
         g = ctx.getChildren()
         l = [next(g).getText() for i in range(ctx.getChildCount())]
         return (int(l[1]), int(l[3]), int(l[5]))
 
-
     # Visit a parse tree produced by SkylineParser#compost.
-    def visitCompost(self, ctx:SkylineParser.CompostContext):
+    def visitCompost(self, ctx: SkylineParser.CompostContext):
         g = ctx.getChildren()
         edificis = []
         for i in range(ctx.getChildCount()):
@@ -93,9 +87,8 @@ class EvalVisitor(ParseTreeVisitor):
                 edificis += [self.visit(ch)]
         return Skyline(edificis)
 
-
     # Visit a parse tree produced by SkylineParser#random.
-    def visitRandom(self, ctx:SkylineParser.RandomContext):
+    def visitRandom(self, ctx: SkylineParser.RandomContext):
         g = ctx.getChildren()
         l = [next(g).getText() for i in range(ctx.getChildCount())]
         return Skyline(int(l[1]), int(l[3]), int(l[5]), int(l[7]), int(l[9]))
